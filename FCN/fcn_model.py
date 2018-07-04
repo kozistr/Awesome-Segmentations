173d5d1bfa09a5fec7572eb08021fc9dd50b6bb3
kozistr <kozistr@gmail.com>
2018-07-04 16:23:29 +0900

    update: initial FCN codes...

--- FCN/fcn_model.py
+++ FCN/fcn_model.py
@@ -1,1 +1,18 @@
-# initial commit
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import tensorflow as tf
+import numpy as np
+
+
+seed = 1337
+
+tf.set_random_seed(seed)
+np.random.seed(seed)
+
+
+class FCN:
+
+    def __init__(self):
+        pass
--- FCN/fcn_train.py
+++ FCN/fcn_train.py
@@ -1,1 +1,32 @@
-# initial commit
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import tensorflow as tf
+import numpy as np
+
+
+seed = 1337
+
+tf.set_random_seed(seed)
+np.random.seed(seed)
+
+
+results = {
+    'output': './gen_img/',
+    'model': './model/FCN-model.ckpt'
+}
+
+train_step = {
+    'batch_size': 16,
+    'global_step': 100001,
+    'logging_interval': 1000,
+}
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
